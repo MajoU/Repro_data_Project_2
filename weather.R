f3e085c9d47c4cd3f903a6d7eab9519f1ebcc3e1
MajoU <majo2906@gmail.com>
2014-08-28 19:44:57 +0200

    Modification of weather.R

--- weather.R
+++ weather.R
@@ -4,7 +4,8 @@ library(data.table) # package 1.9.3
 # DATA PROCESSING
 
 col_names <- c("EVTYPE","FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP")
-data <- data.table(read.csv(bzfile("./repdata_data_StormData.csv.bz2"))[,col_names])
+# data <- data.table(read.csv(bzfile("./repdata_data_StormData.csv.bz2"))[,col_names])
+data <- fread("./Storm_data.csv")
 
 # Change data colnames to lowercase
 setnames(data, names(data), tolower(names(data)))
@@ -15,8 +16,8 @@ data[,evtype := tolower(evtype)]
 head(arrange(data[, .N, by = evtype], desc(N)), 40)
 
 # How much repetitive is some event with similar names?
-head(arrange(data[grep("wind", evtype),.N,by=evtype], desc(N)),20)
-head(arrange(data[grep("flood", evtype),.N,by=evtype], desc(N)),20)
+head(arrange(data[grep("wind", evtype), .N, by = evtype], desc(N)),20)
+head(arrange(data[grep("flood", evtype), .N, by = evtype], desc(N)),20)
 
 # The most influential source of event. I try to join some similar events with
 # influence on health and damage. 
@@ -24,9 +25,9 @@ event <- list("heavy rain|rain", "winter events|winter|frost|cold|snow|freez|ice
     "hurricane events|hurricane|typhoon|tropical|waterspout", "heat|warm", "hail", 
     "flood events|flood|surf|surge", "wind events|wind", "tornado", "rip", "fog", "fire")
 
-# This is complex line that deal with data , grep every element of list within data 
+# this is complex line that deal with data , grep every element of list within data 
 # column evtype and change this grep variables to first element of name from event list 
-# through gsub function. The solution is within the data because data.table package (different than data.frame).
+# through gsub function. the solution is within the data because data.table package (different than data.frame).
 tmp <- lapply(event, function(x) data[grep(x, evtype), evtype := gsub("\\|+[a-z]+","", x)])
 
 # FIRST PART
@@ -36,14 +37,14 @@ harm_sort <- arrange(harm_sum, desc(fatal), desc(injur))
 
 # SECOND PART
 
-prop_token <- data.table(propdmgexp = c("K", "M", "B"), dollars = as.numeric(c("1000", "10e5", "10e8")))
-prop_merge <- merge(setkey(data, propdmgexp), setkey(prop_token, propdmgexp))
-prop_sum <- ddply(prop_merge[, prop := propdmg * dollars],.(evtype), summarize, prop = (sum(prop)))
+prop_token <- data.table(propdmgexp = c("K", "M", "B"), dollars = c("1000", "10^6", "10^9"))
+prop_merge <- merge(setkey(prop_token, propdmgexp), setkey(data, propdmgexp))
+prop_sum <- ddply(prop_merge[, prop := propdmg * dollars],.(evtype), summarize, prop = sum(prop))
 prop_sort <- arrange(prop_sum, desc(prop))
 
-crop_token <- data.table(cropdmgexp = c("K", "M", "B"), dollars = as.numeric(c("1000", "10e5", "10e8")))
-crop_merge <- merge(setkey(data, cropdmgexp), setkey(crop_token, cropdmgexp))
-crop_sum <- ddply(crop_merge[, crop := cropdmg * dollars],.(evtype), summarize, crop = (sum(crop)))
+crop_token <- data.table(cropdmgexp = c("K", "M", "B"), dollars = c("1000", "10^6", "10^9"))
+crop_merge <- merge(setkey(crop_token, cropdmgexp), setkey(data, cropdmgexp))
+crop_sum <- ddply(crop_merge[, crop := cropdmg * dollars],.(evtype), summarize, crop = sum(crop))
 crop_sort <- arrange(crop_sum, desc(crop))
 
 # Alternative
